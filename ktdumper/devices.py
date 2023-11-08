0b08cfc31d8adfde99385937884c31e1111dfcc3
Ilya Zhuravlev <whatever@xyz.is>
2023-11-07 18:23:04 -0600

    add n901is, fix others

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -12,11 +12,15 @@ def MB(x):
 DEVICES = [
     Device("n900i", 0x0409, 0x0112, {
         "dump_nor": NecMemoryDumper("dump_nor.bin", 0x0, MB(32)),
-        "dump_nand": NecNandDumper(size=MB(32)),
+        "dump_nand": NecNandDumper(size=MB(32), quirks=SLOW_READ),
     }),
-    Device("n900is", 0x0409, 0x0144, {
+    Device("n900is", 0x0409, 0x0121, {
         "dump_nor": NecMemoryDumper("dump_nor.bin", 0x0, MB(64)),
         "dump_nand": NecNandDumper(size=MB(32)),
+    }),
+    Device("n901is", 0x0409, 0x0144, {
+        "dump_nor": NecMemoryDumper("dump_nor.bin", 0x0, MB(64)),
+        "dump_nand": NecOnenandDumper(payload_base=0x10000000, onenand_addr=0x06000000, size=MB(64), quirks=SLOW_READ),
     }),
     Device("p900iv", 0x0a3c, 0x000d, {
         "dump_nor": NecMemoryDumper("dump_nor.bin", 0x0, MB(32), quirks=SLOW_READ),
