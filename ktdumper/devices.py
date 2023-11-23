44d95adec7323d09abf75f035901618c2e94c757
Ilya Zhuravlev <whatever@xyz.is>
2023-11-22 18:15:54 -0600

    add dump_nand for p-01b

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -107,7 +107,9 @@ DEVICES = [
 
     Device("p-01b", 0x04da, 0x216b, {
         "dump_rom": PiplExploitMemoryDumper(base=0x0, size=0x8000),
-    }, exploit_flavor="B"),
+        "dump_nand_1": PiplOnenandDumper(onenand_addr=0x0C000000, size=MB(256)),
+        "dump_nand_2": PiplOnenandDumper(onenand_addr=0x18000000, size=MB(256)),
+    }, exploit_flavor="B", payload_base=0x83800000),
 
     Device("p-02b", 0x04da, 0x216b, {
         "dump_rom": PiplExploitMemoryDumper(base=0x0, size=0x8000),
