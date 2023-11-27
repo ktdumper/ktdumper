26a355ff67e1e4623a33d587ebb0f51a074c9275
Ilya Zhuravlev <whatever@xyz.is>
2023-11-26 21:04:01 -0600

    p-01g: add dump emmc

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -165,7 +165,9 @@ DEVICES = [
 
     Device("p-01g", 0x04da, 0x216b, {
         "dump_rom": PiplExploitMemoryDumper(base=0x0, size=0x8000),
-    }, exploit_flavor="C"),
+        "dump_emmc": PiplEmmcDumper(size=MB(2048)),
+    }, exploit_flavor="C", payload_base=0x83800000, emmc_read_and_dcache=0x8000dbf4,
+       usb_command=0x8115a960, usb_data=0x8115a54a, usb_datasz=0x8115a544, usb_respfunc=0x80010a68),
 
     Device("p-01h", 0x04da, 0x216b, {
         "dump_rom": PiplExploitMemoryDumper(base=0x0, size=0x8000),
