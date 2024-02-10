9e68284f2f7b31029a81c0492e7495c2a00ac978
grj1234 <38074422+grj1234@users.noreply.github.com>
2024-02-10 23:14:12 +0900

    Update payload_base value for N703iD
    The NAND of N703iD is not dumpable yet, because the proper onenand_addr value is unknown

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -77,7 +77,7 @@ DEVICES = [
 
     Device("n703id", 0x0409, 0x01aa, {
         "dump_nor": NecMemoryDumper(base=0x0, size=MB(128)),
-    }, payload_base=0x80000000, quirks=SLOW_READ),
+    }, payload_base=0x30000000, quirks=SLOW_READ),
 
     Device("p903i", 0x0a3c, 0x000d, {
         "dump_nor": NecMemoryDumper(base=0x0, size=MB(128)),
