ddfb20ef344339e463004fe45449fcaefd58e258
memory-hunter <59766692+memory-hunter@users.noreply.github.com>
2024-04-23 17:14:17 +0400

    Added N-04A

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -69,6 +69,13 @@ DEVICES = [
     }, payload_base=0x30000000, nand_data=0x10000000, nand_cmd=0x10020000, nand_addr=0x10040000,
        usb_command=0x33ee5198, usb_data=0x33ef51e2, usb_datasz=0x33ef51dc, usb_respfunc=0x519c,
        quirks=SLOW_READ),
+    
+    Device("n-04a", 0x0409, 0x0260, {
+        "dump_nor": NecMemoryDumperPayload(base=0x0, size=MB(128)),
+        "dump_nand": NecNandDumperLp(size=MB(512)),
+    }, payload_base=0x30000000, nand_data=0x10000000, nand_cmd=0x10020000, nand_addr=0x10040000,
+       usb_command=0x33ee5198, usb_data=0x33ef51e2, usb_datasz=0x33ef51dc, usb_respfunc=0x50a8,
+       quirks=SLOW_READ),
 
     Device("n-06a", 0x0409, 0x0274, {
         "dump_nor": NecMemoryDumperPayload(base=0x0, size=MB(128)),
