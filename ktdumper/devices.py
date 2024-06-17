5b38bd31a27d6b97fa0e7a28952a7197ebbc75e0
Ilya Zhuravlev <whatever@xyz.is>
2024-06-17 05:41:39 -0600

    pipl_emmc_fuse: added

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -15,6 +15,7 @@ from dump.pipl_onenand_fast import PiplOnenandFast
 from dump.pipl_emmc_dumper import PiplEmmcDumper
 from dump.sh_exploit import ShExploit
 from dump.fujitsu_java_dumper import FujitsuJavaDumper
+from dump.pipl_emmc_fuse import PiplEmmcFuse
 
 
 def MB(x):
@@ -205,7 +206,8 @@ DEVICES = [
     Device("p-01f", 0x04da, 0x216b, {
         "dump_rom": PiplExploitMemoryDumper(base=0x0, size=0x8000),
         "dump_emmc": PiplEmmcDumper(size=MB(2048)),
-    }, exploit_flavor="C", payload_base=0x83800000, emmc_read_and_dcache=0x8000dba8,
+        "fuse_user": PiplEmmcFuse(offset=0x1a800000, size=0x3fe00000),
+    }, exploit_flavor="C", payload_base=0x83800000, emmc_read_and_dcache=0x8000dba8, emmc_inv_dcache_and_write=0x8000dc80,
        usb_command=0x8115a960, usb_data=0x8115a54a, usb_datasz=0x8115a544, usb_respfunc=0x80010a1c),
 
     Device("p-01g", 0x04da, 0x216b, {
