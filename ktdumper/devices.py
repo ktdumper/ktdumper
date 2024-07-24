baa9f330289802b9ed315da79334aaec6e3e6a68
Ilya Zhuravlev <whatever@xyz.is>
2024-07-24 02:41:13 -0600

    add f-03b

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -443,6 +443,12 @@ DEVICES = [
     Device("f-07a", 0x04c5, 0x115f, {"dump_java": FujitsuJavaDumper()}),
     Device("f-10a", 0x04c5, 0x1162, {"dump_java": FujitsuJavaDumper()}),
 
+    Device("f-03b", 0x04c5, 0x11d8, {
+        "onenand_id": ShSrecExploitOnenandId(),
+        "dump_nand": ShSrecExploitOnenandDumper(size=MB(512), has_4k_pages=True),
+    }, payload_base=0xE55B0000, fatal_err=0x60c04848, usb_interrupt=0x60C02000, usb_getch=0x60c03f14, usb_send=0x60c04118, usb_send_commit=0x60c03a94,
+        onenand_addr=0x30000000),
+
     Device("f-08c", 0x04c5, 0x122f, {
         "onenand_id": ShSrecExploitOnenandId(),
         "dump_nand": ShSrecExploitOnenandDumper(size=MB(512), has_4k_pages=True),
