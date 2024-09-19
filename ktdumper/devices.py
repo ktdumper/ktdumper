227138689a9bd65d517ff8e10500a28f7a63acab
Unabandonware <126729444+Unabandonware@users.noreply.github.com>
2024-09-19 00:48:56 -0400

    Add N-03A, 820N dump_nand (#43)

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -191,11 +191,11 @@ DEVICES = [
        quirks=SLOW_READ),
 
     Device("n-03a", 0x0409, 0x0268, {
-        "dump_nor": NecMemoryDumperPayload(base=0x0, size=MB(128)),
-        "dump_nand": NecNandDumperLp(size=MB(512)),
-    }, payload_base=0x30000000, nand_data=0x10000000, nand_cmd=0x10020000, nand_addr=0x10040000,
-       usb_command=0x33ee50d0, usb_data=0x33ef511a, usb_datasz=0x33ef5114, usb_respfunc=0x582c,
-       quirks=SLOW_READ),
+        "dump_nor": NecMemoryDumper_v2(base=0x0, size=MB(128)),
+        "onenand_id": NecOnenandId_v2(),
+        "dump_nand": NecOnenandFast_v2(),
+    }, payload_base=0x30000000, usb_receive=0x00004e5c, usb_send=0x000057b4,
+       onenand_addr=0x10000000),
 
     Device("n-04a", 0x0409, 0x0260, {
         "dump_nor": NecMemoryDumperPayload(base=0x0, size=MB(128)),
@@ -301,11 +301,11 @@ DEVICES = [
     # SOFTBANK
 
     Device("820n", 0x0409, 0x0250, {
-        "dump_nor": NecMemoryDumperPayload(base=0x0, size=MB(128)),
-        "dump_nor_slow": NecMemoryDumper(base=0x0, size=MB(128)),
-    }, payload_base=0x30000000,
-       usb_command=0x33eec3f8, usb_data=0x33efc442, usb_datasz=0x33efc43c, usb_respfunc=0x4f58,
-       quirks=SLOW_READ),
+        "dump_nor": NecMemoryDumper_v2(base=0x0, size=MB(128)),
+        "onenand_id": NecOnenandId_v2(),
+        "dump_nand": NecOnenandFast_v2(),
+    }, payload_base=0x30000000, usb_receive=0x00004f94, usb_send=0x000058ec,
+       onenand_addr=0x10000000),
 
     Device("930n", 0x0409, 0x027c, {
         "dump_nor": NecMemoryDumperPayload(base=0x0, size=MB(128)),
