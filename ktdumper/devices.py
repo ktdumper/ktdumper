a706905efe8179c20aa9e6fdfd4b09a9e1e3248c
Unabandonware <126729444+Unabandonware@users.noreply.github.com>
2024-09-29 05:58:21 -0400

    Add SH-11C (#50)

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -876,6 +876,12 @@ DEVICES = [
     }, payload_base=0xE55B0000, fatal_err=0x60c045fc, usb_interrupt=0x60c02000, usb_getch=0x60c03d28, usb_send=0x60c03f00, usb_send_commit=0x60c038fc,
        nand_data=0x16000000, nand_addr=0x16000010, nand_cmd=0x16000020),
 
+    Device("sh-11c", 0x04dd, 0x940e, {
+        "dump_nand": ShSrecExploitMlbaDumper_v2(),
+        "nand_id": ShSrecExploitNandId_v2(),
+    }, payload_base=0xE55B0000, fatal_err=0x60c048dc, usb_interrupt=0x60c02000, usb_getch=0x60c03fa8, usb_send=0x60c041ac, usb_send_commit=0x60c03b28,
+       nand_data=0x16000000, nand_addr=0x16000010, nand_cmd=0x16000020),
+    
     Device("sh-07f", 0x04dd, 0x9464, {
         "jump_symbian": ShExploit(jump_dst=0x50803630),
     }),
