a5da3aefd05576b0c4938cffce0dcdaf4af94c66
Yu <agwbty@gmail.com>
2025-03-11 23:41:59 +0800

    Add P-05C (#79)

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -738,6 +738,13 @@ DEVICES = [
     }, exploit_flavor="C2", payload_base=0x83800000, usb_receive=0x80024768, usb_send=0x80024554,
        onenand_addr=0x0C000000),
 
+    Device("p-05c", 0x04da, 0x216b, {
+        "dump_rom": PiplExploitMemoryDumper(base=0x0, size=0x8000),
+        "dump_nand": PiplOnenandDumper(),
+        "onenand_id": PiplOnenandId(),
+    }, exploit_flavor="C", payload_base=0x83800000, onenand_addr=0x0C000000,
+       usb_command=0x8115a960, usb_data=0x8115a54a, usb_datasz=0x8115a544, usb_respfunc=0x80027f6c),
+
     Device("p-06c", 0x04da, 0x216b, {
         "dump_rom": PiplExploitMemoryDumper(base=0x0, size=0x8000),
         "dump_nand_peek_poke": PiplOnenandDumper(),
