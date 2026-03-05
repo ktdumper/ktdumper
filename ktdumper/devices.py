319f9d3122a2f00f30732bb15588d2e01c5d2a97
Unabandonware <126729444+Unabandonware@users.noreply.github.com>
2026-03-04 22:00:44 -0500

    Add N900iL

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -136,6 +136,11 @@ DEVICES = [
         "dump_nand": NecNandDumper(size=MB(32), quirks=SLOW_READ),
     }, payload_base=0x10000000, nand_data=0x04000000, nand_cmd=0x04000800, nand_addr=0x04000400),
 
+    Device("n900il", 0x0409, 0x0125, {
+        "dump_nor": NecMemoryDumper(base=0x0, size=MB(64)),
+        "dump_nand": NecNandDumper(size=MB(32), quirks=SLOW_READ),
+    }, payload_base=0x10000000, nand_data=0x04000000, nand_cmd=0x04000800, nand_addr=0x04000400),
+
     Device("n700i", 0x0409, 0x0140, {
         "dump_nor": NecMemoryDumper(base=0x0, size=MB(64)),
         "dump_nand": NecNandDumper(size=MB(32), quirks=SLOW_READ),
