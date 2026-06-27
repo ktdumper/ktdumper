19018bd8f19e643fea36541fd5bda05b05c029cc
Unabandonware <admin@unabandonware.com>
2026-06-27 00:00:48 -0400

    Add D851iWM

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -1632,6 +1632,7 @@ DEVICES = [
     Device("d901is", 0x06d3, 0x20a0, {"dump_java": FujitsuJavaDumperAlternative()}),
     Device("d701i", 0x06d3, 0x20c0, {"dump_java": FujitsuJavaDumperAlternative()}),
     Device("d701iwm", 0x06d3, 0x20d0, {"dump_java": FujitsuJavaDumperAlternative()}),
+    Device("d851iwm", 0x06d3, 0x20e0, {"dump_java": FujitsuJavaDumper()}),
     Device("d902i", 0x06d3, 0x20b0, {"dump_java": FujitsuJavaDumper()}),
     Device("d902is", 0x06d3, 0x2120, {"dump_java": FujitsuJavaDumper()}),
     Device("d702i", 0x06d3, 0x2100, {"dump_java": FujitsuJavaDumper()}),
