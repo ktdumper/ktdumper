1c0e629753097ee25673005bbeea92887b72dc86
Unabandonware <admin@unabandonware.com>
2026-08-25 13:05:13 -0400

    typo fix

--- ktdumper/dump/sony/sony_mdoc_dumper_v2.py
+++ ktdumper/dump/sony/sony_mdoc_dumper_v2.py
@@ -3,7 +3,7 @@ from dump.v2.mdoc_dumper_v2 import MdocDumper_v2
 from dump.sony.sony_protocol_v2 import SonyProtocol_v2
 
 
-class SonyMdocDumper_v2(SonyProtocol_v2):
+class SonyMdocDumper_v2(MdocDumper_v2, SonyProtocol_v2):
 
     def read_sector(self, part, sector):
         self.usb_send(struct.pack("<BBI", 0x80, part, sector))
