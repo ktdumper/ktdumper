2ca25c1fb7ddb8425cb335df491d64baf426655f
Unabandonware <admin@unabandonware.com>
2026-08-25 14:10:43 -0400

    Update sony_mdoc_dumper_slow_v2.py

--- ktdumper/dump/sony/sony_mdoc_dumper_slow_v2.py
+++ ktdumper/dump/sony/sony_mdoc_dumper_slow_v2.py
@@ -1,9 +1,5 @@
-import struct
-from dump.sony.sony_mdoc_dumper_slow_v2 import SonyMdocDumperSlow_v2
+from dump.v2.mdoc_dumper_v2 import MdocDumper_v2
+from dump.sony.sony_protocol_v2 import SonyProtocol_v2
 
-class SonyMdocDumper_v2(SonyMdocDumperSlow_v2):
-
-    def read_sector(self, part, sector):
-        self.usb_send(struct.pack("<BBI", 0x80, part, sector))
-        data = self.usb_receive()
-        return data
+class SonyMdocDumperSlow_v2(MdocDumper_v2, SonyProtocol_v2):
+    pass
